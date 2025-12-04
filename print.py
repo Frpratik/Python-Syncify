b2a33d1acc014ff28b06a4acb164e9037caea260
Pratik <ghugepratik52@gmail.com>
2025-12-04 20:45:19 +0530

    Initial Commit

---
+++ print.py
@@ -0,0 +1,54 @@
+# print using double quotes and single quotes
+print("This is our first program in Python using double quotes")
+
+print('This is our first program in Python using single quotes')
+
+# print strings with quotes inside
+print("He said, 'Python is awesome!'")
+print('She replied, "Indeed, it is!"')
+
+# print numbers (no need for quotes)
+print(42)
+print(23)
+
+# print multiple items
+print("The answer is", 42)   # ,(comma) => default seprator and default value is space
+print("Sum of", 20, "and", 22, "is", 42)
+print("Pratik", "loves", "Python")
+print(23,45,67,89)
+
+
+# print with custom separator
+# Sep => To add sep by our own
+# We can use any string as separator
+# We can use single quotes or double quotes for sep value
+print("Pratik","Loves","Coding", sep="-")
+print(22,23,24, sep=">>>>>>")
+
+
+
+# Printing without newline
+# End => To add end by our own
+# We can use single quotes or double quotes for end value
+# We can use any string as end value
+print("Hello", end=" ")
+print("World")  # Both print on same line
+
+
+# Empty print (creates blank line)
+print()
+print("------------------------------------")
+
+
+# Print using special characters
+print("My name is Pratik\nI love coding")  # \n creates new line
+print("Hello\tAasim")  # \t creates tab space
+
+
+
+
+
+
+
+
+
